404434c1613cf5ce3ecb736a31fefcc1712146b9
plkujaw <jawor.jakub@gmail.com>
2020-05-20 20:06:17 +0100

    solved

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,19 +1,39 @@
+# frozen_string_literal: true
+
 def atm(n)
   notes = []
-  if n == 10
-    (n / 10).times { notes << 10 }
-  elsif n == 20
-    (n / 20).times { notes << 20 }
-  elsif n == 50
-    (n / 50).times { notes << 50 }
-  elsif n == 100
-    (n / 100).times { notes << 100 }
-  elsif n == 200
-    (n / 200).times { notes << 200 }
-  elsif n == 500
+
+  return -1 if n % 10 != 0
+
+  if n >= 500
     (n / 500).times { notes << 500 }
+    n = n % 500
   end
 
-  p notes
-  notes
+  if n >= 200
+    (n / 200).times { notes << 200 }
+    n = n % 200
+  end
+
+  if n >= 100
+    (n / 100).times { notes << 100 }
+    n = n % 100
+  end
+
+  if n >= 50
+    (n / 50).times { notes << 50 }
+    n = n % 50
+  end
+
+  if n >= 20
+    (n / 20).times { notes << 20 }
+    n = n % 20
+  end
+
+  if n >= 10
+    (n / 10).times { notes << 10 }
+    n = n % 10
+  end
+
+  notes.length
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 require 'atm'
 
 describe 'atm' do
-it 'checks how many notes of each amount' do
-  expect(atm(10)).to eq([10])
-  expect(atm(20)).to eq([20])
-  expect(atm(50)).to eq([50])
-  expect(atm(100)).to eq([100])
-  expect(atm(200)).to eq([200])
-  expect(atm(500)).to eq([500])
-end
+  it 'checks minimum number of notes to withdraw the amount and return -1 if not possible' do
+    expect(atm(10)).to eq(1)
+    expect(atm(20)).to eq(1)
+    expect(atm(30)).to eq(2)
+    expect(atm(40)).to eq(2)
+    expect(atm(50)).to eq(1)
+    expect(atm(100)).to eq(1)
+    expect(atm(200)).to eq(1)
+    expect(atm(500)).to eq(1)
+    expect(atm(770)).to eq(4)
+    expect(atm(125)).to eq(-1)
+    expect(atm(-1)).to eq(-1)
+  end
 end
