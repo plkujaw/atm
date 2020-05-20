edd85c292da99f9c2a5a7ccaa45c8d5df2dc46a6
plkujaw <jawor.jakub@gmail.com>
2020-05-20 20:23:45 +0100

    added initial tests

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -17,3 +17,16 @@ describe 'atm' do
     expect(atm(-1)).to eq(-1)
   end
 end
+
+
+# expect(atm(10)).to eq([10])
+# expect(atm(20)).to eq([20])
+# expect(atm(30)).to eq([20, 10])
+# expect(atm(40)).to eq([20, 20])
+# expect(atm(50)).to eq([50])
+# expect(atm(100)).to eq([100])
+# expect(atm(200)).to eq([200])
+# expect(atm(500)).to eq([500])
+# expect(atm(770)).to eq([500, 200, 50, 20])
+# expect(atm(125)).to eq(-1)
+# expect(atm(-1)).to eq(-1)
